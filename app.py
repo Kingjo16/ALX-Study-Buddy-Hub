b4db1571cd7c7a1b170f8a5c8146989a8dc26cc0
Kidus <kingkidy50@gmail.com>
2024-06-12 01:52:22 -0700

    new addition

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
+import os
 from flask import Flask, render_template
 from routes.auth import auth
 from routes.groups import group
@@ -8,7 +11,7 @@ from routes.forums import forums
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'micku101820'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
 
 
 # Register the auth routes
@@ -19,12 +22,18 @@ app.register_blueprint(groups)
 app.register_blueprint(forums)
 app.register_blueprint(forum)
 
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Error handling
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/forms.py
+++ app/forms.py
@@ -27,4 +27,3 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
